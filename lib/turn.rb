db0eaca234fb1d5f7b7b25423be3946edf9a5be5
David Kim <davidjkim810@gmail.com>
2017-12-11 21:05:34 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,4 +22,6 @@ def valid_move?(board, index)
   index.between?(0,8) && position_taken?(board, index) == false
 end
 
-def move(board, index, character = "X")
+def move(board, index, character == "X")
+
+end
