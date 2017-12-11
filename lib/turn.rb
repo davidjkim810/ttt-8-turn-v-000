9d8c3cadd38ca8f1b078d0de0613b38e02f08fbd
David Kim <davidjkim810@gmail.com>
2017-12-11 21:03:43 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,11 @@ end
 def input_to_index(user_input)
   position = user_input.to_i - 1
 end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && position_taken?(board, index) == false
+end
