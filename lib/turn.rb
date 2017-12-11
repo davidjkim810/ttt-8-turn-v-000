30169cdd2850255d29a1701209c1bf29f50fde9d
David Kim <davidjkim810@gmail.com>
2017-12-11 20:31:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,5 @@
 def display_board(board)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 row1 = ["#{board[0]}", "|", "#{board[1]}", "|", "#{board[2]}"]
 row2 = ["#{board[3]}", "|", "#{board[4]}", "|", "#{board[5]}"]
 row3 = ["#{board[6]}", "|", "#{board[7]}", "|", "#{board[8]}"]
