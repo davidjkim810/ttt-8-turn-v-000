df993d071ff225ba133ddcfa501fce675ee57ed6
David Kim <davidjkim810@gmail.com>
2017-12-11 20:50:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def display_board(board)
-row1 = ["#{board[0]}", "|", "#{board[1]}", "|", "#{board[2]}"]
-row2 = ["#{board[3]}", "|", "#{board[4]}", "|", "#{board[5]}"]
-row3 = ["#{board[6]}", "|", "#{board[7]}", "|", "#{board[8]}"]
+row1 = "#{board[0]} | #{board[1]} | #{board[2]}"
+row1 = "#{board[3]} | #{board[4]} | #{board[5]}"
+row1 = "#{board[6]} | #{board[7]} | #{board[8]}"
 divide = ["-----------"]
 puts row1
 puts divide
