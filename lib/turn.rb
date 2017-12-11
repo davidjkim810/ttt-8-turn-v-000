6705b84e92def6432f4d064f2167d640cabe55e9
David Kim <davidjkim810@gmail.com>
2017-12-11 20:57:19 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,15 @@
 def display_board(board)
-row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
-row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
-row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
-divide = "-----------"
-puts row1
-puts divide
-puts row2
-puts divide
-puts row3
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  divide = "-----------"
+  puts row1
+  puts divide
+  puts row2
+  puts divide
+  puts row3
+end
+
+def input_to_index(input)
+
 end
